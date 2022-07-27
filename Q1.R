5d7d80f90e5866e011b7d33b00835f470ccbfb7f
Indunil Aravinda <indunilaravindapro@gmail.com>
2022-07-28 01:36:44 +0530

    Finding Missing Values

---
+++ Q1.R
@@ -0,0 +1,12 @@
+#Pre-Processing
+
+#load the tidyverse
+library(tidyverse)
+
+#setting work directory
+setwd("CW/ML CW")
+
+#load the dataset
+onlineSell <- read_csv("Live.csv")
+
+View(onlineSell)
