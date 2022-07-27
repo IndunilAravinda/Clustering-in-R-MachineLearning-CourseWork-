3ac7334b358a1a5908657308c45335ccca65bbac
Indunil Aravinda <indunilaravindapro@gmail.com>
2022-07-28 05:14:19 +0530

    Dropping empty columns

--- Q1.R
+++ Q1.R
@@ -6,4 +6,12 @@ library(tidyverse)
 #load the dataset
 onlineSell <- read_csv("Live.csv")
 
+#view the summary
 summary(onlineSell)
+
+#dropping empty columns
+onlineSell %>%
+  select(-c(Column1,Column2,Column3,Column4))
+  
+
+summary(onlineSell)  
