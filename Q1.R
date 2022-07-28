61aaf972d2d2166c486a230e4760b650b8f21141
Indunil Aravinda <indunilaravindapro@gmail.com>
2022-07-28 06:42:56 +0530

    Converting categorical data into integers

--- Q1.R
+++ Q1.R
@@ -6,12 +6,19 @@ library(tidyverse)
 #load the dataset
 onlineSell <- read_csv("Live.csv")
 
-#view the summary
-summary(onlineSell)
-
 #dropping empty columns
-onlineSell %>%
+onlineSell <- onlineSell %>%
   select(-c(Column1,Column2,Column3,Column4))
-  
 
+#Converting categorical values into integers
+onlineSell <- onlineSell %>%
+  mutate(status_type=unclass(as.factor(onlineSell$status_type)))
+
+#checck the unique instance for status_id
+unique(onlineSell$status_id)
+summary(unique(onlineSell$status_id))
+
+#view the summary
+view(onlineSell)
 summary(onlineSell)  
+
